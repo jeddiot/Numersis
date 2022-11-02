1fcb6133228021003b5e0ec0418e6d3c3e70fdd7
just another normie <46043618+jeddiot@users.noreply.github.com>
2022-11-02 14:17:52 +0800

    Create 7.3_gauss_seidel_method.py

---
+++ 7.3_gauss_seidel_method.py
@@ -0,0 +1,35 @@
+# Defining our function as seidel which takes 3 arguments
+# as A matrix, Solution and B matrix
+   
+def seidel(a, x ,b):
+    #Finding length of a(3)       
+    n = len(a)                   
+    # for loop for 3 times as to calculate x, y , z
+    for j in range(0, n):        
+        # temp variable d to store b[j]
+        d = b[j]                  
+          
+        # to calculate respective xi, yi, zi
+        for i in range(0, n):     
+            if(j != i):
+                d-=a[j][i] * x[i]
+        # updating the value of our solution        
+        x[j] = d / a[j][j]
+    # returning our updated solution           
+    return x    
+   
+# int(input())input as number of variable to be solved                
+n = 4                              
+a = []                            
+b = []        
+# initial solution depending on n(here n=4)                     
+x = [0, 0, 0, 0]                        
+a = [[10, -1, 2, 0],[-1, 11, -1, 3],[2, -1, 10, -1],[0, 3, -1, 8]]
+b = [6, 25, -11, 15]
+print(x)
+  
+#loop run for m times depending on m the error value
+for i in range(0, 25):            
+    x = seidel(a, x, b)
+    #print each time the updated solution
+    print(x)         
