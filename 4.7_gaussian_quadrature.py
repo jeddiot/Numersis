39ca124b0b8c88e232eef08b64a289145d5a343c
just another normie <46043618+jeddiot@users.noreply.github.com>
2022-11-10 00:04:56 +0800

    Update 4.7_gaussian_quadrature.py

--- 4.7_gaussian_quadrature.py
+++ 4.7_gaussian_quadrature.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# simple version of gauss quadrature, including the function using "np.polynomial.legendre.leggauss(n)".
 def gauss(f,a,b,n): # f is the function, a and b indicates the interval to integrate [a,b], n is th degree of the Legendre polynomials
     half = float(b-a)/2.
     mid = (a+b)/2.
@@ -17,9 +18,9 @@ def fun(x):
 for i in range(1,10):  
     print(gauss(fun,0,1,i))
 
-# another version of gauss quadrature, modified from matlab code by Peter J. Acklam. Not including the function needed to be quadratured.
-
-def gaussquad(*args): # save the variables in a tuple called "args", in the order of n, b, a
+# advanced version of gauss quadrature, modified from matlab code by Peter J. Acklam. Not including the function needed to be quadratured.
+def gaussquad(*args): 
+    # save the variables in a tuple called "args", in the order of n, b, a
     u = []
     vtsort = []
     a = np.zeros((args[0], args[0]))
@@ -33,6 +34,9 @@ def gaussquad(*args): # save the variables in a tuple called "args", in the orde
         # gaussquad(N, C) assumes the interval is from 0 to C.
         b = (0,)
         args += b
+    #print(args)
+    else:
+        return
 
     # Same as in matlab, A = diag(u, -1) + diag(u, 1), but faster (no addition).
     for i in range(args[0] - 1):
@@ -40,6 +44,7 @@ def gaussquad(*args): # save the variables in a tuple called "args", in the orde
         a[i + 1,k] = u[i]
         a[k,i + 1] = u[i]
         k += 1
+    #print(a)
 
     # Find the base points X and weight factors W for the interval [-1,1].
     vl, vt = np.linalg.eig(np.array(a)) # vl = eigen values of a, the column of vt = eigen vectors of a.
@@ -47,13 +52,19 @@ def gaussquad(*args): # save the variables in a tuple called "args", in the orde
     for i in np.argsort(vl): # get the index for sorting vl
         vtsort.append(vt[0,i]) # get the first element for each eigen vectors
     w = 2 * np.transpose(vtsort)**2
+    #print(w)
+    #print(vlsort)
     
     # Linearly transform from [-1,1] to [a,b].
     vlsort = (args[1] - args[2]) / 2 * vlsort + (args[2] + args[1]) / 2
-
-    if len(args) != 0:
+ 
+    if len(vlsort) and len(vtsort): # if they exist
         xx = np.transpose(vlsort)
-        ww = np.transpose(vtsort)
+        ww = np.transpose(w)
+        print(xx)
+        print(ww)
         return
-
     return [xx, ww]
+
+# driver's code
+gaussquad(5)
